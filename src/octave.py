becd3207ac05c81e93d1829f710f846be007ff69
pt12lol <pt12lol@gmail.com>
2013-12-18 23:35:41 +0100

    Octave constructor was redefined.

--- src/octave.py
+++ src/octave.py
@@ -30,30 +30,39 @@ class Octave(object):
         return self.__repr__()
 
     def __eq__(self, other):
-        return (self.size, self.lines) == (other.size, other.lines)
+        return (self.lines, self.size) == (other.lines, other.size)
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
     def __lt__(self, other):
-        return octaveSizes.index(self.size) < octaveSizes.index(other.size) \
-            if octaveSizes.index(self.size) != octaveSizes.index(other.size) \
+        return sizes.index(self.size) < sizes.index(other.size) \
+            if sizes.index(self.size) != sizes.index(other.size) \
             else (self.lines < other.lines if self.size == 'lower'
                   else self.lines > other.lines)
 
-    def __add__(self, num):
-        if self.size == 'lower' and num >= 0:
-            return Octave('lower', self.lines + num)
-        if self.size == 'upper' and num <= 0:
-            return Octave('upper', self.lines - num)
-        if self.size == 'lower' and num < 0:
-            if self.lines + num < 0:
-                return Octave('upper', -(self.lines + num) - 1)
-            return Octave('lower', self.lines + num)
-        if self.lines - num < 0:
-            return Octave('lower', -(self.lines + num) - 1)
-        return Octave('upper', self.lines + num)
+    def __add__(self, other):
+        if self.size == 'lower' and other >= 0:
+            return Octave(self.lines + other, 'lower')
+        if self.size == 'upper' and other <= 0:
+            return Octave(self.lines - other, 'upper')
+        if self.size == 'lower' and other < 0:
+            if self.lines + other < 0:
+                return Octave(-(self.lines + other) - 1, 'upper')
+            return Octave(self.lines + other, 'lower')
+        if self.lines - other < 0:
+            return Octave(-self.lines + other - 1, 'lower')
+        return Octave(self.lines + other, 'upper')
 
-    def __sub__(self, num):
-        return self.__add__(-num)
+    def __sub__(self, other):
+        if type(other) == int:
+            return self.__add__(-other)
+        result = 0;
+        if self < other:
+            while self < other + result:
+                result -= 1
+        elif self > other:
+            while self > other + result:
+                result += 1
+        return result
 
