e2c747ff7dd75ef64cdc9e86418e30dad35ec7a3
pt12lol <pt12lol@gmail.com>
2013-12-18 23:36:31 +0100

    Tests for new Octave constructor has been added.

--- tests/testOctave.py
+++ tests/testOctave.py
@@ -10,10 +10,10 @@ class TestOctave(unittest.TestCase):
 
     def setUp(self):
         self.o1 = Octave()
-        self.o2 = Octave('lower', 0)
-        self.o3 = Octave('upper', 0)
-        self.o4 = Octave('upper', 1)
-        self.o5 = Octave('lower', 1)
+        self.o2 = Octave(0)
+        self.o3 = Octave(0, 'upper')
+        self.o4 = Octave(1, 'upper')
+        self.o5 = Octave(1, 'lower')
 
     def testRepr(self):
         self.assertEqual(str(self.o1), "'")
@@ -67,18 +67,20 @@ class TestOctave(unittest.TestCase):
     def testAdd(self):
         tmp = copy(self.o1)
         tmp += 1
-        self.assertEqual(tmp, Octave('lower', 2))
-        self.assertEqual(tmp + 2, Octave('lower', 4))
-        self.assertEqual(tmp + (-3), Octave('upper', 0))
-        self.assertEqual(tmp + (-2), Octave('lower', 0))
-        self.assertEqual(tmp + (-4), Octave('upper', 1))
+        self.assertEqual(tmp, Octave(2))
+        self.assertEqual(tmp + 2, Octave(4, 'lower'))
+        self.assertEqual(tmp + (-3), Octave(0, 'upper'))
+        self.assertEqual(tmp + (-2), Octave(0, 'lower'))
+        self.assertEqual(tmp + (-4), Octave(1, 'upper'))
 
     def testSub(self):
         tmp = copy(self.o1)
         tmp -= -1
-        self.assertEqual(tmp, Octave('lower', 2))
-        self.assertEqual(tmp - (-2), Octave('lower', 4))
-        self.assertEqual(tmp - 3, Octave('upper', 0))
-        self.assertEqual(tmp - 2, Octave('lower', 0))
-        self.assertEqual(tmp - 4, Octave('upper', 1))
+        self.assertEqual(tmp, Octave(2))
+        self.assertEqual(tmp - (-2), Octave(4, 'lower'))
+        self.assertEqual(tmp - 3, Octave(0, 'upper'))
+        self.assertEqual(tmp - 2, Octave(0, 'lower'))
+        self.assertEqual(tmp - 4, Octave(1, 'upper'))
+        self.assertEqual(tmp - Octave(0), 2)
+        self.assertEqual(tmp - Octave(4), -2)
 
