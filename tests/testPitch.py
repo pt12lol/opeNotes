7673d0ad5164063ff62eaf4e5d35ac218a85e53f
pt12lol <pt12lol@gmail.com>
2013-12-20 20:47:37 +0100

    Tests for pitches comparisons have been updated.

--- tests/testPitch.py
+++ tests/testPitch.py
@@ -10,22 +10,27 @@ class TestPitch(unittest.TestCase):
 
     def setUp(self):
         self.p1 = Pitch()
-        self.p2 = Pitch('d', 2, 0)
+        self.p2 = Pitch('a', 0, 0)
         self.p3 = Pitch.fromOctave('c', -3, Octave(0))
         self.p4 = Pitch('b', 4, 0, 'upper')
         self.p5 = Pitch('a', 0, 1, 'lower')
         self.p6 = Pitch.fromOctave('c', 1, Octave(0))
+        self.p7 = Pitch('c', 2, 0)
 
     def testRepr(self):
         self.assertEqual(str(self.p1), "a'")
-        self.assertEqual(str(self.p2), "dis")
+        self.assertEqual(str(self.p2), "a")
         self.assertEqual(str(self.p3), "ceseh")
         self.assertEqual(str(self.p4), "bisis,")
         self.assertEqual(str(self.p5), "a'")
+        self.assertEqual(str(self.p6), "cih")
+        self.assertEqual(str(self.p7), "cis")
 
     def testEq(self):
         self.assertEqual(self.p1, self.p5)
+        self.assertEqual(self.p4, self.p7)
         self.assertTrue(self.p1 == self.p5)
+        self.assertTrue(self.p7 == self.p4)
         self.assertFalse(self.p2 == self.p4)
         self.assertFalse(self.p3 == self.p4)
         self.assertFalse(self.p3 == self.p5)
@@ -35,36 +40,41 @@ class TestPitch(unittest.TestCase):
         self.assertTrue(self.p2 != self.p3)
         self.assertTrue(self.p2 != self.p4)
         self.assertFalse(self.p1 != self.p5)
+        self.assertFalse(self.p4 != self.p7)
 
     def testLt(self):
         self.assertLess(self.p2, self.p1)
         self.assertTrue(self.p3 < self.p2)
-        self.assertTrue(self.p4 < self.p3)
+        self.assertTrue(self.p3 < self.p4)
         self.assertFalse(self.p5 < self.p4)
         self.assertFalse(self.p1 < self.p5)
         self.assertFalse(self.p6 < self.p3)
+        self.assertFalse(self.p4 < self.p7)
 
     def testGt(self):
         self.assertGreater(self.p1, self.p2)
         self.assertTrue(self.p2 > self.p3)
-        self.assertTrue(self.p3 > self.p4)
+        self.assertTrue(self.p4 > self.p3)
         self.assertFalse(self.p4 > self.p5)
         self.assertFalse(self.p1 > self.p5)
         self.assertFalse(self.p3 > self.p6)
+        self.assertFalse(self.p4 > self.p7)
 
     def testLe(self):
         self.assertLessEqual(self.p2, self.p1)
         self.assertLessEqual(self.p5, self.p1)
+        self.assertLessEqual(self.p4, self.p7)
         self.assertTrue(self.p3 <= self.p2)
-        self.assertTrue(self.p4 <= self.p3)
+        self.assertTrue(self.p3 <= self.p4)
         self.assertTrue(self.p1 <= self.p5)
         self.assertFalse(self.p5 <= self.p4)
         self.assertFalse(self.p6 <= self.p3)
 
     def testGe(self):
         self.assertGreaterEqual(self.p1, self.p2)
+        self.assertGreaterEqual(self.p4, self.p7)
         self.assertTrue(self.p2 >= self.p3)
-        self.assertTrue(self.p3 >= self.p4)
+        self.assertTrue(self.p4 >= self.p3)
         self.assertTrue(self.p1 >= self.p5)
         self.assertFalse(self.p4 >= self.p5)
         self.assertFalse(self.p3 >= self.p6)
