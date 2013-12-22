9477b411efb6c2293f07f7158b7f830b21894dcb
pt12lol <pt12lol@gmail.com>
2013-12-22 13:00:08 +0100

    Interval added to package.

--- opeNotes/__init__.py
+++ opeNotes/__init__.py
@@ -2,4 +2,5 @@ from opeNotes.octave import Octave, octaveSizes
 from opeNotes.pitch import Pitch, pitchNames, scaleRels
 from opeNotes.rhythmicValue import RhythmicValue
 from opeNotes.note import Note
+from opeNotes.interval import Interval, intervalTypes
 
