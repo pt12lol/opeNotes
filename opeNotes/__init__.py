4a94c2bb0afb9b0c7427d166659042744b4ff234
pt12lol <pt12lol@gmail.com>
2013-12-25 14:41:55 +0100

    Template for lilypond added.

--- opeNotes/__init__.py
+++ opeNotes/__init__.py
@@ -3,5 +3,17 @@ from opeNotes.pitch import Pitch, pitchNames, semitones
 from opeNotes.rhythmicValue import RhythmicValue
 from opeNotes.note import Note
 from opeNotes.interval import Interval, intervalTypes, scaleRels
-from opeNotes.chord import Chord
+from opeNotes.pitchesStructure import PitchesStructure
+from opeNotes.party import Party
+
+lyTemplate = r"""\version "2.16.2"
+\score
+{
+    \new Staff << %s >>
+    %s
+}
+"""
+
+lyPdfTemplate = lyTemplate % ('%s', '')
+lyMidiTemplate = lyTemplate % ('%s', r'\midi { \context { \Staff } }')
 
